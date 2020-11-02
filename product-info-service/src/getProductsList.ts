3e1bd88bcc67feac1b733868afe50ae8adcdd22f
Viktoryia_Yalavaya <Viktoryia_Yalavaya@epam.com>
2020-11-02 05:11:55 +0300

    feature/add-lambda: Create product-service

--- product-info-service/src/getProductsList.ts
+++ product-info-service/src/getProductsList.ts
@@ -1,12 +1,24 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
+import products from './productService.json';
 
-export const getProductsList: APIGatewayProxyHandler = async (event, _context) => {
+export const getProductsList: APIGatewayProxyHandler = async () => {
+    const headers: any = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+    };
+    let statusCode: number = 200;
+    let body: string = '';
+    try {
+        body = JSON.stringify(products, null, 2);
+    } catch (e) {
+        body = JSON.stringify(e, null, 2);
+        statusCode = 500;
+    }
+
     return {
-        statusCode: 200,
-        body: JSON.stringify({
-            message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-            input: event,
-        }, null, 2),
+        statusCode,
+        body,
+        headers,
     };
 }
